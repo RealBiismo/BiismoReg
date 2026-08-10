48b4bade3bf933cec2c35bd0b7cf0b72a00b15c1
RealBiismo <realbiismo@gmail.com>
2026-08-10 13:50:06 +0100

    Refresh PWA cache for homepage layout fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "biismo-reg-v23";
+const CACHE_NAME = "biismo-reg-v24";
 const CANONICAL_ORIGIN = "https://biismoreg.com";
 const LEGACY_HOSTS = new Set(["biismoreg-com.onrender.com"]);
 const NETWORK_FIRST_ASSETS = new Set([
